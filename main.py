b4c018aba7ed160055dde62cd291a16dfff9acf2
cmike221 <cmike221@yandex.ru>
2024-08-15 10:36:55 +0300

    Домашнее задание в файле main

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 import time
 
+
 def start_browser():
-    firefox_options = Options()
-    firefox_options.add_argument("--headless")  # Открыть браузер в фоновом режиме
+#    firefox_options = Options()
+#    firefox_options.add_argument("--headless")  # Открыть браузер в фоновом режиме
 #    service = Service('C:/geckodriver/')  # Укажите путь к вашему GeckoDriver
 #    browser = webdriver.Firefox(service=service, options=firefox_options)
     browser = webdriver.Firefox()
     return browser
+
 
 def search_wikipedia(browser, query):
     browser.get('https://www.wikipedia.org/')
@@ -19,13 +21,15 @@ def search_wikipedia(browser, query):
     search_box.submit()
     time.sleep(2)  # Подождать, пока страница загрузится
 
+
 def print_paragraphs(browser):
     paragraphs = browser.find_elements(By.CSS_SELECTOR, 'p')
     for i, paragraph in enumerate(paragraphs):
         print(f"Параграф {i + 1}: {paragraph.text}\n")
-        user_input = input("Нажмите Ввод для перелистывания или пробел+ввод для вызова меню: ")
-        if user_input.lower() == ' ':
+        user_input = input("Нажмите Ввод для перелистывания или введите цифру 0 для вызова меню: ")
+        if user_input.lower() == '0':
             break
+
 
 def list_links(browser):
     links = browser.find_elements(By.CSS_SELECTOR, '#bodyContent a')
@@ -36,6 +40,7 @@ def list_links(browser):
             print(f"{i + 1}: {link.text}")
             link_map[i + 1] = href
     return link_map
+
 
 def main():
     browser = start_browser()
@@ -50,7 +55,7 @@ def main():
             while True:
                 print("\nВыберите действие:")
                 print("1. Листать параграфы текущей статьи")
-                print("2. Перейти на выбранную статью")
+                print("2. Выбрать другую статью из списка")
                 print("3. Выйти из программы")
                 choice = input("Ваш выбор: ")
 
@@ -70,8 +75,6 @@ def main():
     finally:
         browser.quit()
 
+
 if __name__ == "__main__":
     main()
-
-
-
